2aa67cf0b2d94f6df88e69f566f173ea72c248d8
honu <honu@nate.com>
2018-09-15 21:40:48 +0900

    adding second to the counter

--- Components/Timer/presenter.js
+++ Components/Timer/presenter.js
@@ -3,15 +3,40 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Button from '../Button'
 
 class Timer extends Component {
+
+  componentWillReceiveProps(nextProps) {
+
+    const currentProps = this.props;
+
+    if (currentProps.isPlaying == false 
+      && nextProps.isPlaying == true) {
+      const timerInteval = setInterval(() => {
+        currentProps.addSecond()
+      }, 1000);
+
+      this.setState({
+        timerInteval
+      });
+
+      // start the interval
+    } else if (currentProps.isPlaying == true 
+      && nextProps.isPlaying == false) {
+      // stop the interval
+      clearInterval(this.state.timerInteval)
+    }
+  }
+
   render() {
-    const { 
+    console.log(this.props)
+    const {
       isPlaying,
-      elapsedTime, 
+      elapsedTime,
       timerDuration,
       startTimer,
-      restartTimer
+      restartTimer,
+      addSecond
     } = this.props;
-    console.log(this.props)
+
     return (
       <View style={styles.container}>
         <StatusBar style={"light-content"} />
@@ -19,8 +44,8 @@ class Timer extends Component {
           <Text style={styles.time}>25:00</Text>
         </View>
         <View style={styles.lower}>
-          { !isPlaying ? (<Button iconName="play-circle-o" onPress={startTimer} />) : null}
-          { isPlaying ? (<Button iconName="stop-circle-o" onPress={restartTimer} />) : null}
+          {!isPlaying ? (<Button iconName="play-circle-o" onPress={startTimer} />) : null}
+          {isPlaying ? (<Button iconName="stop-circle-o" onPress={restartTimer} />) : null}
         </View>
       </View>
     );
